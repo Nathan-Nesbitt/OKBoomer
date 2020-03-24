6b4e0f334609dd2c4e26e263f2f482090b53b95f
Emily Medema <medema.emily@gmail.com>
2020-03-24 15:50:34 -0700

    populated scroll view for bio and pics on match page

--- High Fidelity Prototype/app/src/main/java/com/example/okboomerprototype/specific_chat.java
+++ High Fidelity Prototype/app/src/main/java/com/example/okboomerprototype/specific_chat.java
@@ -8,15 +8,13 @@ import android.widget.TextView;
 
 public class specific_chat extends AppCompatActivity {
     Intent intent;
-    TextView t;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specific_chat);
-        t = findViewById(R.id.textView4);
         intent = getIntent();
         String userName = intent.getStringExtra("name");
-        t.setText(userName);
 
     }
 }
