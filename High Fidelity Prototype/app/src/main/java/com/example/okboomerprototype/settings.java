3040e7ea3d1b033ffa8dd3cf67860ef72c15cb5c
Emily Medema <medema.emily@gmail.com>
2020-03-27 23:13:06 -0700

    working on map, for some reason it is only showing a beige screen with the google logo

--- High Fidelity Prototype/app/src/main/java/com/example/okboomerprototype/settings.java
+++ High Fidelity Prototype/app/src/main/java/com/example/okboomerprototype/settings.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class settings extends AppCompatActivity {
-
+    EditText radius;
+    int rad = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        radius = (EditText) findViewById(R.id.editText7);
+        String rad = radius.getText().toString();
+        this.rad = Integer.parseInt(rad);
     }
     public void toProfile(View view){
         Intent intent = new Intent(this, profilePage.class);
@@ -28,5 +33,10 @@ public class settings extends AppCompatActivity {
     public void saveChanges(View view){
         save_dialogue save = new save_dialogue();
         save.show(getSupportFragmentManager(), "save changes");
+    }
+    public void toMap (View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtra("radius", rad);
+        startActivity(intent);
     }
 }
